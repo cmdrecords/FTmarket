62ca73265399b07b767772b11c5eabc62401de96
cmdrecords <f.50tunit@mail.ru>
2026-05-26 10:46:00 +0300

    add js count visitors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
-let x = "Fogiman";
+
+let countVisitors;
+
+document.addEventListener('DOMContentLoaded', () => {
+    countVisitors = Number(localStorage.getItem("count"));
+    countVisitors++;
+    localStorage.setItem("count", countVisitors);
+    console.log(countVisitors);
+});
